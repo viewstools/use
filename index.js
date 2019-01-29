94116986444826cd6694183ad431b87bcc7e4b6e
Darío Javier Cravero <dario@uxtemple.com>
2019-01-29 09:28:21 +0000

    fix: separate text and capture base styles

--- index.js
+++ index.js
@@ -337,6 +337,52 @@ body,
   height: 100%;
   margin: 0;
 }
+.views-block,
+#root {
+  align-items: stretch;
+  background-color: transparent;
+  border-radius: 0;
+  border: 0;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  hyphens: auto;
+  margin: 0;
+  margin: 0;
+  overflow-wrap: break-word;
+  padding: 0;
+  padding: 0;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  word-wrap: break-word;
+}
+.views-text {
+  box-sizing: border-box;
+  hyphens: auto;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+}
+.views-capture {
+  box-sizing: border-box;
+  border-radius: 0;
+  border: 0;
+  hyphens: auto;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+}
+.views-capture::-moz-focus-inner {
+  border: 0;
+  margin: 0;
+  padding: 0;
+}
+/* remove number arrows */
+.views-capture[type='number']::-webkit-outer-spin-button,
+.views-capture[type='number']::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
 .views-block, #root {
   align-items: stretch;
   box-sizing: border-box;
